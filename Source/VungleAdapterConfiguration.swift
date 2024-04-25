ecfe877bbcbbabc4559c9607882bcffed0c12a95
Daniel Barros <daniel.barros@chartboost.com>
2024-04-25 13:41:21 -0700

    Add public keyword

--- Source/VungleAdapterConfiguration.swift
+++ Source/VungleAdapterConfiguration.swift
@@ -10,18 +10,18 @@ import VungleAdsSDK
 @objc public class VungleAdapterConfiguration: NSObject {
 
     /// The version of the partner SDK.
-    @objc static var partnerSDKVersion: String {
+    @objc public static var partnerSDKVersion: String {
         VungleAds.sdkVersion
     }
 
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    @objc static let adapterVersion = "4.7.3.0.0"
+    @objc public static let adapterVersion = "4.7.3.0.0"
 
     /// The partner's unique identifier.
-    @objc static let partnerID = "vungle"
+    @objc public static let partnerID = "vungle"
 
     /// The human-friendly partner name.
-    @objc static let partnerDisplayName = "Vungle"
+    @objc public static let partnerDisplayName = "Vungle"
 }
