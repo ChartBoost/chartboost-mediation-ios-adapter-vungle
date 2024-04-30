836b4cf8cbd7bb3cb3fcf6eb73e81af5243eafc4
Daniel Barros <dani.barros@me.com>
2024-04-30 10:56:09 -0700

    Add consent status override method (#67)

--- Source/VungleAdapterConfiguration.swift
+++ Source/VungleAdapterConfiguration.swift
@@ -4,6 +4,7 @@
 // license that can be found in the LICENSE file.
 
 import Foundation
+import os.log
 import VungleAdsSDK
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
@@ -24,4 +25,28 @@ import VungleAdsSDK
 
     /// The human-friendly partner name.
     @objc public static let partnerDisplayName = "Vungle"
+
+    /// Use to manually set the consent status on the Pangle SDK.
+    /// This is generally unnecessary as the Mediation SDK will set the consent status automatically based on the latest consent info.
+    @objc public static func setGDPRStatusOverride(_ status: Bool) {
+        isGDPRStatusOverriden = true
+        VunglePrivacySettings.setGDPRStatus(status)
+        os_log(.info, log: log, "Vungle SDK GDPR status override set to %{public}s", "\(status)")
+    }
+
+    /// Use to manually set the consent status on the Pangle SDK.
+    /// This is generally unnecessary as the Mediation SDK will set the consent status automatically based on the latest consent info.
+    @objc public static func setCCPAStatusOverride(_ status: Bool) {
+        isCCPAStatusOverriden = true
+        VunglePrivacySettings.setCCPAStatus(status)
+        os_log(.info, log: log, "Vungle SDK CCPA status override set to %{public}s", "\(status)")
+    }
+
+    /// Internal flag that indicates if the GDPR status has been overriden by the publisher.
+    static private(set) var isGDPRStatusOverriden = false
+
+    /// Internal flag that indicates if the CCPA status has been overriden by the publisher.
+    static private(set) var isCCPAStatusOverriden = false
+
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.vungle", category: "Configuration")
 }
